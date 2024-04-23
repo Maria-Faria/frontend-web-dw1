e35a89713e03950c41803b9530a11df64be09e97
Maria-Faria <mariaeduardadefaria15@gmail.com>
2024-04-22 22:13:28 -0300

    adicionando cards com JS

--- if-finance/js/main.js
+++ if-finance/js/main.js
@@ -1,11 +1,11 @@
-console.log('Arquivo JS externo');
+//console.log('Arquivo JS externo');
 
 //let cria uma variável (não permite recriar uma mesma variável no bloco)
 //var cria uma variável (permite recriar uma mesma variável no bloco)
 //const cria uma constante
 
 //exemplo escopo de bloco
-let nome = 'Maria';
+/*let nome = 'Maria';
 
 if (true) {
     let nome = 'Pedro';
@@ -71,15 +71,15 @@ const meuNome = nomeCompleto ? "Maria Eduarda" : "Maria"; //condicao ? T : F
     return valor1 + valor2;
 }*/
 
-const soma = (valor1, valor2) => {
+/*const soma = (valor1, valor2) => {
     return valor1 + valor2;
-}
+}*/
 
 /*const soma = function (valor1, valor2) {
     return valor1 + valor2;
 }*/
 
-const mostrar = 'idade';
+/*const mostrar = 'idade';
 
 const user = {
     nome: "Maria",
@@ -94,9 +94,61 @@ const user = {
     consoles: ['PS5', 'Switch', {teste: 10, valor: 50}]
 };
 
+user.email = "mariaeduarda@gmail.com";
+user.nomeDaMae = "Angela";
+
 console.log(user['email']); //user.email
 console.log(user.endereco.bairro);
 
 console.log(user.soma(3, 4));
 console.log(user.consoles[2].valor);
 console.log(user[mostrar]);
+
+console.error('Deu ruim')
+console.log(user.nomeDaMae);
+console.log(user);
+
+/*const newUser = {
+    ...user, 
+    endereco: {
+        rua: "Av. Bahia",
+        numero: 20,
+        bairro: "Indaiá"
+    }
+}*/
+
+/*const newUser = {
+    ...user.endereco, rua: "Av Mato Grosso"
+}
+
+console.log(newUser);*/
+
+function addCard() {
+    const listaCards = document.querySelector('#cards');
+    listaCards.innerHTML = listaCards.innerHTML + `
+    <div class="card-ticker">
+
+                    <header>
+                        <h2><span>NASDAQ:</span> AAPL</h2>
+                        <h1>Apple Inc</h1>
+                    </header>
+
+                    <main>
+                        <p>R$ 250,80</p>
+                        <span>▲ 0,35%</span><span> R$0,60</span>
+                    </main>
+
+                    <footer>
+                        <div>
+                            <p>40</p>
+                            <span>Ações</span>
+                        </div>
+
+                        <div>
+                            <p>R$ 34853,45</p>
+                            <span>Posição</span>
+                        </div>
+                    </footer>
+
+                </div>`
+}
